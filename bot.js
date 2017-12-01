e149d20488211c119fdb6c482b84b4b8e50e6d50
Mikulasz12 <mikolajek1206@gmail.com>
2017-12-01 20:09:08 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,28 @@
 var Discord = require("discord.js");
 var bot = new Discord.Client();
-var prefix = "==";
+var prefix = "!";
 
 bot.on("ready", () => {
 	console.log("I am ready!");
-	bot.user.setGame('')
-});
-
-bot.on('ready', () => {
-    console.log('I am ready!');
+	bot.user.setGame('Hiya! !cmds')
 });
 
 bot.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+	if(message.author.bot) return;
+	if(!message.content.startsWith(prefix)) return;
+
+	if(message.content.startsWith(prefix + "cmds")) {
+		message.channel.sendMessage('Here are the commands: **motd - Message of the day!**');
+	} else
+
+	if(message.content.startsWith(prefix + "motd")) {
+		message.channel.sendMessage('This bot is in beta!');
+	}
+
+
+	
 });
+
 
 
 bot.login(process.env.BOT_TOKEN);
