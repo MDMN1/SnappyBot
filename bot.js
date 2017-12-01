39fbb3c67f01b6e0133d0e48e51da1d92d85da91
Mikulasz12 <mikolajek1206@gmail.com>
2017-12-01 20:06:57 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,22 +7,15 @@ bot.on("ready", () => {
 	bot.user.setGame('')
 });
 
-bot.on('message', message => {
-	if(message.author.bot) return;
-	if(!message.content.startsWith(prefix)) return;
-
-	if(message.content.startsWith(prefix + "cmds")) {
-		message.channel.sendMessage('Here are the commands: **motd - Message of the day!**');
-	} else
-
-	if(message.content.startsWith(prefix + "motd")) {
-		message.channel.sendMessage('This bot is in beta!');
-	}
-
-
-	
+bot.on('ready', () => {
+    console.log('I am ready!');
 });
 
+bot.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
 
 bot.login(process.env.BOT_TOKEN);
