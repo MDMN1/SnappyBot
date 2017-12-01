ce36c0d86a339549c83f27d30c029cc588798ab3
Mikulasz12 <mikolajek1206@gmail.com>
2017-12-01 20:35:20 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,19 +4,34 @@ var prefix = "!";
 
 bot.on("ready", () => {
 	console.log("I am ready!");
-	bot.user.setGame('Hiya! !cmds')
+	bot.user.setGame('Type !cmds')
 });
 
 bot.on('message', message => {
 	if(message.author.bot) return;
 	if(!message.content.startsWith(prefix)) return;
 
-	if(message.content.startsWith(prefix + "cmds")) {
-		message.channel.sendMessage('Here are the commands: **motd - Message of the day!**');
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
+
+	let args = message.content.split(" ").slice(1);
+
+	if (command === "add") {
+		let num1 = parseInt(args[0]);
+		let num2 = parseInt(args[1]);
+		message.channel.sendMessage(num1 + num2)
 	} else
 
-	if(message.content.startsWith(prefix + "motd")) {
-		message.channel.sendMessage('');
+	if (command === "say") {
+		message.channel.sendMessage(args.join(" "));
+	} else
+
+	if (command === "cmds") {
+		message.channel.sendMessage('Here are the commands! \n**motd - Message of the day!** \n**Add - Add a number! ==add 5 6**');
+	} else
+
+	if (command === "motd") {
+		message.channel.sendMessage('This bot is in beta!');
 	}
 
 
