c4ebe3e3beb9bc03240b253d2fea0a466881124b
Mikulasz12 <mikolajek1206@gmail.com>
2017-12-02 11:14:30 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
 	if(newMember.user.presence.game && newMember.user.presence.game.name === "ROBLOX") {
 		newMember.addRole(playRole);
 	} else if(!newMember.user.presence.game && newMember.roles.has(playrole.id)) {
-		newMember.removerole(playRole);
+		newMember.removeRole(playRole);
 	}
 });
 
