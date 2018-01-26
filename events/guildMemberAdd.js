c2a927a89d3aabad64c56573b9413f7fc08b3d92
Mikulasz12 <mikolajek1206@gmail.com>
2018-01-26 18:59:23 +0000

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,6 @@ module.exports = (client, member) => {
   client.log('Log', `${member.user.tag} (${member.id}) has joined ${member.guild.name} (${member.guild.id})`);
   const welcomeChannel = member.guild.channels.find('name', 'welcome');
   if (welcomeChannel) {
-    welcomeChannel.send(`Please welcome ${member.user.tag} to our wonderful guild!`);
+    welcomeChannel.send(`Please welcome ${member.user.tag} to our wonderful server!`);
   }
 };
