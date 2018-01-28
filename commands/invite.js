df887b1be2033eb47857bfbaf29c4d21854c0dc1
Mikulasz12 <mikolajek1206@gmail.com>
2018-01-28 14:42:04 +0000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = (client, message) => {
   const embed = new Discord.MessageEmbed()
     .setThumbnail(message.guild.iconURL())
-    .setFooter('Please check your direct messages.')
+    .setFooter('!invite')
     .addfield('User Input', 'Check your direct messages!')
     .setColor(0x16A085);
   message.channel.send({ embed });
