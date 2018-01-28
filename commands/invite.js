0b1ef69786ba56216e43f000f5e15c5f69f35bb0
Mikulasz12 <mikolajek1206@gmail.com>
2018-01-28 14:44:09 +0000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ module.exports = (client, message) => {
   const embed = new Discord.MessageEmbed()
     .setThumbnail(message.guild.iconURL())
     .setFooter('!invite')
-    .addfield('User Input', 'Check your direct messages!')
+    .addField('User Input', 'Check your direct messages!')
     .setColor(0x16A085);
   message.channel.send({ embed });
   message.author.send('The bot is not ready to get released to the public!');
