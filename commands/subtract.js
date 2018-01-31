f9e97aba688c74f52ac49652bfb7d28e9a837143
Mikulasz12 <mikolajek1206@gmail.com>
2018-01-31 19:13:30 +0000

    Add files via upload

---
+++ commands/subtract.js
@@ -0,0 +1,6 @@
+module.exports = (client, message, args) => {
+  const numArray = args.map(n => parseInt(n));
+  const total = numArray.reduce( (p, c) => p - c);
+
+  message.channel.send(total);
+};
