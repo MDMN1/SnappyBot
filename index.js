5581fb481305cd9787ac05b32d3b244ebb36683f
Mikulasz12 <mikolajek1206@gmail.com>
2018-01-31 19:14:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,14 @@ client.commands.set('exe', require('./commands/exe.js'));
 client.commands.set('cmds', require('./commands/cmds.js'));
 client.commands.set('8ball', require('./commands/8ball.js'));
 client.commands.set('invite', require('./commands/invite.js'));
+client.commands.set('add', require('./commands/add.js'));
+client.commands.set('multiply', require('./commands/multiply.js'));
+client.commands.set('divide', require('./commands/divide.js'));
+client.commands.set('subtract', require('./commands/subtract.js'));
 
 client.on('message', message => require('./events/message.js')(client, message));
 client.on('guildCreate', guild => require('./events/guildCreate.js')(client, guild));
 client.on('ready', () => require('./events/ready.js')(client));
 client.on('guildMemberAdd', member => require('./events/guildMemberAdd.js')(client, member));
 
-client.login(process.env.BOT_TOKEN);
+client.login(client.config.token);
