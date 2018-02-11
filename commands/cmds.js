fa8ec05ff8c0eebcedce8cc1a2af731886810c92
Mikulasz12 <mikolajek1206@gmail.com>
2018-02-11 15:25:29 +0000

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = (client, message) => {
   const embed = new Discord.MessageEmbed() 
-    .addField('Commands', '8ball\nmultiply\nadd\nsubtract\ndivide\nannounce - You nee a role called "Announce Permission"\ncmds\nexe - Owner only\ninvite\nping\nsay - Owner only')
+    .addField('Commands', '8ball\nmultiply\nadd\nsubtract\ndivide\nannounce - You need a role called "Announce Permission"\ncmds\nexe - Owner only\ninvite\nping\nsay - Owner only')
     .setColor(0xFF5233)
     .setFooter('Not case sensitive.');
 
