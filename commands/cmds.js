8851ea3a4e542405388a8c7b06dff795eca2bc9c
Mikulasz12 <mikolajek1206@gmail.com>
2018-02-12 14:30:00 +0000

    Add files via upload

--- commands/cmds.js
+++ commands/cmds.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = (client, message) => {
   const embed = new Discord.MessageEmbed()
-    .addField('Commands', '8ball - Ask a question and it will predict your future!\nmultiply\nadd\nsubtract\ndivide\nannounce - You need a role called "Announce Permission"\ncmds\nexe - Owner only\ninvite - Invites the bot to your server\nping - Checks the Latency along with the API Latency\nabout - About the bot\nsay - Owner only')
+    .addField('Commands', '8ball - Ask a question and it will predict your future!\nmultiply\nadd\nsubtract\ndivide\nannounce - You need a role called "Announce Permission"\ncmds\nexe - Owner only\ninvite - Invites the bot to your server\nping - Checks the Latency along with the API Latency\nabout - About the bot\nmyinfo - Displays your info\nsay - Owner only')
     .setColor(0xFF5233)
     .setFooter('Not case sensitive.');
   message.channel.send({ embed });
---
+++ commands/myinfo.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+module.exports = (client, message) => {
+  const embed = new Discord.MessageEmbed()
+    .addField(message.author.username, 'These are your stats.')
+    .addField('Full Username:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('ID', message.author.id)
+    .addField('Created At', message.author.createdAt)
+    .addField('Hobby', 'Helping people.');
+  message.channel.send({ embed });
+};
