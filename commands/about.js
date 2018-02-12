e7885746510b8714af417863523b108cd077033d
Mikulasz12 <mikolajek1206@gmail.com>
2018-02-12 13:48:14 +0000

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = (client, message) => {
   const embed = new Discord.MessageEmbed()
     .setColor(0xFF33AF)
-    .addField('About', 'This bot was made by Mikulasz12. He is trying to improve the bot and make it more awesome!')
-    .addField('Info', '\n**Default Prefix**: !\n**Host**: Heroku');
+    .addField('About', 'This bot was made by Mikulasz12#7131 (245983842672967680). He is trying to improve the bot and make it more awesome!')
+    .addField('Info', '\n**Default Prefix**: !\n**Host**: Heroku\n');
   message.channel.send({ embed });
 };
