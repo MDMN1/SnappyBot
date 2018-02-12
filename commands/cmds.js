3f9ebfc785e148d3c70bd6a0fa174e75a6641de2
Mikulasz12 <mikolajek1206@gmail.com>
2018-02-12 13:44:25 +0000

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -5,7 +5,5 @@ module.exports = (client, message) => {
     .addField('Commands', '8ball - Ask a question and it will predict your future!\nmultiply\nadd\nsubtract\ndivide\nannounce - You need a role called "Announce Permission"\ncmds\nexe - Owner only\ninvite - Invites the bot to your server\nping - Checks the Latency along with the API Latency\nabout - About the bot\nsay - Owner only')
     .setColor(0xFF5233)
     .setFooter('Not case sensitive.');
-
-
   message.channel.send({ embed });
 };
