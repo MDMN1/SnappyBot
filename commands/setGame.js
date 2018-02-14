5a64d5cbe9496edfcea1752d415d6f45a6fc25b1
Mikulasz12 <mikolajek1206@gmail.com>
2018-02-14 10:39:24 +0000

    Add files via upload

---
+++ commands/setGame.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports = (client, message, args) => {
+  if (message.author.id !== client.config.ownerID) return;
+  const status = realstatus = args.join(' ');
+  if (status.length === 0) {
+    const embed = new Discord.MessageEmbed()
+      .setColor(0xFF0000)
+      .setDescription(':x: Your argument can\'t be empty.');
+    message.channel.send({ embed });
+    return;
+  }
+  client.user.setActivity(`${status}`);
+  const embed = new Discord.MessageEmbed()
+    .setColor(0x64E01D)
+    .setDescription(':white_check_mark: Done, changes should now take effect! Sometimes, they might not.');
+  message.channel.send({ embed });
+};
