7ae14bd12fed4bb5d43829f6e4f57808323383ab
Mikulasz12 <mikolajek1206@gmail.com>
2018-02-14 14:40:51 +0000

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,4 @@
 module.exports = (client, guild, message) => {
   client.log('Log', `I have been added to the guild: ${guild.name}, Owned by: ${guild.owner.user.tag}, with ${guild.memberCount} members.`);
-  message.channel.send('Hello, thanks for adding me! In order for the bot to function properly, you have to add these channels: #action-logs, #reports. If you want me to kick users, you will have to give me ban and kick permission.');
+  message.defaultChannel.send('Hello, thanks for adding me! In order for the bot to function properly, you have to add these channels: #action-logs, #reports. If you want me to kick users, you will have to give me ban and kick permission.');
 };
