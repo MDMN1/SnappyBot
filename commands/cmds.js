15c4c7a602f7e7ede56e618b73089b8fec2f51dc
Mikulasz12 <mikolajek1206@gmail.com>
2018-02-14 13:52:44 +0000

    Add files via upload

--- commands/cmds.js
+++ commands/cmds.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = (client, message) => {
   const embed = new Discord.MessageEmbed()
-    .addField('Commands', '8ball - Ask a question and it will predict your future!\nmultiply\nadd\nsubtract\ndivide\nannounce - You need a role called "Announce Permission"\ncmds\nexe - Owner only\ninvite - Invites the bot to your server\nping - Checks the Latency along with the API Latency\nabout - About the bot\nmyinfo - Displays your info\nKick - Kicks a user (manage_members permission)\nBan - Bans a user (manage_members permission)\nsay - Owner only\nReport - Reports a user\nCat - MEOW MEOW HISS! Shows a random cute cat! \nDog - BARK BARK BITE! Shows a random cute dog!\nsetgame - Owner only, sets the status of the bot (what game he is playing)')
+    .addField('Commands', '8ball - Ask a question and it will predict your future!\nmultiply\nadd\nsubtract\ndivide\nannounce - You need a role called "Announce Permission"\ncmds\nexe - Owner only\ninvite - Invites the bot to your server\nping - Checks the Latency along with the API Latency\nabout - About the bot\nmyinfo - Displays your info\nKick - Kicks a user (manage_members permission)\nBan - Bans a user (manage_members permission)\nsay - Owner only\nReport - Reports a user\nCat - MEOW MEOW HISS! Shows a random cute cat! \nDog - BARK BARK BITE! Shows a random cute dog!\nsetgame - Owner only, sets the status of the bot (what game he is playing)\nDice - Picks a number from 1 to 10.')
     .setColor(0xFF5233)
     .setFooter('Not case sensitive.');
   message.channel.send({ embed });
