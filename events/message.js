d5ab4f6e508bb0d16bf0b577f0dcec5b9247ce91
Mikulasz12 <mikolajek1206@gmail.com>
2018-02-23 15:06:16 +0000

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -1,26 +1,7 @@
-const fs = require('fs');
-
 module.exports = (client, message) => {
-  const prefixes = JSON.parse(fs.readFileSync('./Storage/prefixes.json'), 'utf8');
-
-  if (!prefixes[message.guild.id]) {
-    prefixes[message.guild.id] = {
-      prefixes: client.config.prefix,
-    };
-  }
-
-  const prefixcheck = prefixes[message.guild.id].prefixes || client.config.prefix;
-
-  if (!prefixes[message.guild.id]) {
-    prefixes[message.guild.id] = {
-      prefixes: client.config.prefix,
-    };
-  }
-
   if (message.author.bot) return;
-  if (!message.content.startsWith(prefixcheck)) return;
 
-  if (message.content.indexOf(prefixcheck) !== 0) return;
+  if (message.content.indexOf(client.config.prefix) !== 0) return;
 
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
